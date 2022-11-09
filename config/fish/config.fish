3dd18384fb2ee953167ae47036b98104650ab66c
ohm <ohmrc2545@gmail.com>
2022-11-09 17:13:09 -0500

    add thunar, starship and fish

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+starship init fish | source
+set -U fish_greeting
+fish_add_path ~/.local/bin/
+# neofetch --config ~/.config/bspwm/neofetch/bigfetch.conf
+rxfetch
+
+alias ls="exa --icons -l"
+alias df="duf"
